c87ea610fcad831d747045f62c691ae0493e8a0f
Moaud Naciri <mouadnaciri1@gmail.com>
2021-04-07 15:17:05 +0100

    Implement unmerging a manga two-page spread with the next #mangaplus

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const DEFAULT_PAGE_CLASSNAME_MOUSEOVER = 'msm-ext__mouseover';
+export type HTMLElementOrNone = HTMLElement | undefined | null;
--- src/pages_merger/mangaplus_pages_merger.ts
+++ src/pages_merger/mangaplus_pages_merger.ts
@@ -1,4 +1,6 @@
+import {HTMLElementOrNone} from '../constants';
 import {Page, PagesFinder} from '../pages_finder/pages_finder';
+import {nearestAncestor} from '../utils/dom_traversal';
 import {PagesMerger} from './pages_merger';
 
 export class MangaplusPagesMerger implements PagesMerger {
@@ -14,7 +16,36 @@ export class MangaplusPagesMerger implements PagesMerger {
   }
 
   unmerge(page: Page) {
-    throw new Error('Not implemented yet');
+    const parent: HTMLElement | null = page.parentElement;
+    if (!parent) {
+      throw new Error("Invalid state: page doesn't have a parent container.");
+    }
+    const nextPage: Page = parent.children[0] as HTMLElement;
+    const pageIndex: number = this.pagesFinder.indexOf(page);
+    if (pageIndex === -1) {
+      throw new Error('Invalid argument: page not foundin the document.');
+    }
+
+    const container: HTMLElementOrNone = nearestAncestor(page, 'zao-container');
+    if (!container) {
+      throw new Error("Invalid state: couldn't find parent container.");
+    }
+    const surfaceContainer: HTMLElementOrNone = container.parentElement;
+    if (!surfaceContainer) {
+      throw new Error("Invalid state: couldn't find parent pages container.");
+    }
+    if (!surfaceContainer.classList.contains('zao-surface')) {
+      throw new Error("Invalid state: couldn't find `.zao-surface` container.");
+    }
+    const nextPageParent: HTMLElementOrNone = container.nextElementSibling?.querySelector(
+      '.zao-image-container'
+    );
+    if (!nextPageParent) {
+      throw new Error(
+        "Invalid state: next page doesn't have a parent container."
+      );
+    }
+    nextPageParent.append(nextPage);
   }
 
   isMerged(page: Page): boolean {
